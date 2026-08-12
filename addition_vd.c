13b2fc2590dac0698b460f28451cb0f505e3719c
vriddhi30 <VRIDDHI.36020@stu.upes.ac.in>
2026-08-12 23:06:28 +0530

    DAY 1 Programming

---
+++ addition_vd.c
@@ -0,0 +1,11 @@
+// Write a program to input two numbers and display their sum.
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    printf("enter the values of a and b") ;
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("result=%d",c);
+    return 0;
+
+}
