817f2ea8631c44d0adda8c4df618fa56096d1ce6
vriddhi30 <VRIDDHI.36020@stu.upes.ac.in>
2026-08-24 03:27:30 +0530

    DAY 15 Programming

---
+++ Factorial.c
@@ -0,0 +1,18 @@
+// Write a program to calculate the factorial of a numbeR
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+
+    printf("Factorial = %d", fact);
+
+    return 0;
+}
