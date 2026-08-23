30def3f82dc7eda2fde766b5eacad4cb240374be
vriddhi30 <VRIDDHI.36020@stu.upes.ac.in>
2026-08-23 23:44:07 +0530

    DAY 14 Programming

---
+++ Product_n.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, product = 1;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i = i + 2) {
+        product = product * i;
+    }
+
+    printf("Product = %d", product);
+
+    return 0;
+}
